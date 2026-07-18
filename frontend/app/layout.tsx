c75cbee8a7082539c4d695942a783383c660f932
Muhammad Usman <usmighani1921@gmail.com>
2026-07-18 17:18:24 +0500

    update layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -7,7 +7,7 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+congit st geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
